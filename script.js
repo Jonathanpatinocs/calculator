ebd49aaeddb9606b164426a7427b00cc07c6b50c
Jonathanpatinocs <jonathanpatinocs@gmail.com>
2024-05-10 16:08:39 -0400

    add and subtraction logic + html changes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let displayNum = 0;
+
 let currentSymbol = '';
 let x = '';
 let y = 0;
@@ -7,22 +7,47 @@ const add_button = document.getElementById('add');
 const subtract_button = document.getElementById('subtract');
 const multiply_button = document.getElementById('multiply');
 const divide_button = document.getElementById('divide');
+const equal_button = document.getElementById('equal');
 const numbers = document.getElementsByClassName('numbers');
+const displayNum = document.getElementById('displayNumber');
+const displaySymbol = document.getElementById('displaySymbol');
+
 
 function get_num() {
-    let num = this.innerHTML;
-    
+    if (currentSymbol == 'equals') {
+        x = ''
+        y = 0;
+        currentSymbol = ''
+        displaySymbol.innerHTML = '';
+    }
+    num = this.innerHTML;
     x += num;
+    displayNum.innerHTML = x;
     console.log(x);
 }
 for (let i = 0; i < numbers.length; i++ ) {
     numbers[i].addEventListener('click', get_num);
 }
 add_button.addEventListener('click', ()=>  {
+    if (currentSymbol !== '') {
+        equals(currentSymbol);
+    }
+    y = x;
+    x = '';
     currentSymbol = 'add';
+    displaySymbol.innerHTML = "+";
 })
 subtract_button.addEventListener('click', ()=>  {
+    if (currentSymbol !== '') {
+        console.log(currentSymbol);
+        equals(currentSymbol)
+    }
+    y = x;
+    x = '';
     currentSymbol = 'subtract';
+    displaySymbol.innerHTML = '-';
+    console.log(y);
+    
 })
 multiply_button.addEventListener('click', ()=>  {
     currentSymbol = 'multipy';
@@ -30,32 +55,50 @@ multiply_button.addEventListener('click', ()=>  {
 divide_button.addEventListener('click', ()=>  {
     currentSymbol = 'divide';
 })
+equal_button.addEventListener('click', ()=> {
+    equals(currentSymbol);
+    displayNum.innerHTML = x;
+    currentSymbol = 'equals';
+    displaySymbol.innerHTML = '=';
+} )
 
-function add(x, y, ...args) {
+function equals(currentSymbol) {
+    switch (currentSymbol) {
+        case 'add': {
+            x = add(+x,+y);
+            console.log(x);
+            break;
+        }
+        case 'subtract': {
+            x = subtract(+y,+x);
+            console.log(x);
+            break;
+        }
+        case 'equals': {
+            break;
+        }
+    }
+}
+
+// ----------------- Math Operator Functions -------------------
+function add(x, y,) {
     let sum = x + y;
-    for (const arg of args) {
-        sum += arg;
-    }
     return sum;
 }
-function subtract(x, y, ...args) {
+function subtract(x, y) {
     let sum = x - y;
-    for(const arg of args) {
-        sum -= arg;
-    }
+    
     return sum;
 }
-function multiply(x, y , ...args) {
+function multiply(x, y ) {
     let sum = x * y;
-    for (const arg of args ) {
-        sum *= arg;
-    }
+    
     return sum;
 }
 function divide(x, y) {
     let sum = x / y;
     return sum;
 }
-console.log(add_button)
+
 
 
